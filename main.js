bd9fcbf96fd89a3a4b432571f956ba97b20519af
Edric Barnwal <187942690+edricbarnwal@users.noreply.github.com>
2026-05-17 19:13:14 +0530

    Resolve merge conflicts in style.css, projects.json, and main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const projectCardList = document.querySelector(".card-list");
 
 
@@ -41,47 +40,3 @@ initApp();
 
 
 
-=======
-const projectCardList = document.querySelector(".card-list");
-
-
-let projectDetail = [];
-
-const showCard = () => {
-
-    projectDetail.forEach(project => {
-
-    const projectCard  = document.createElement("div");
-    projectCard.classList.add('project-card')
-
-    projectCard.innerHTML = `
-    
-    <img src="${project.image}" alt="">
-        <div>
-          <h3>${project.name}</h3>
-        <p>${project.description}</p>
-        <a href="${project.link}">View</a>
-        </div>
-        `
-
-    projectCardList.appendChild(projectCard)
-
-    });
-
-}
-
-const initApp = () => {
-    fetch('projects.json').then
-    (Response => Response.json()).then
-    (data => {
-        projectDetail = data;
-        showCard()
-    })
-}
-
-initApp();
-
-
-
-
->>>>>>> 3e48a084c9568e5934ea196c13e06caf25b718a5
